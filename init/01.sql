7e865c10ecb4fe10c378e7cc8d42d85dc127acae
Ravalesh Bhat <ravaleshb@gmail.com>
2021-09-02 23:57:07 +0530

    DockerComposeTest

---
+++ init/01.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Course 
+(
+    CourseID int,
+    CourseName varchar(1000),
+    Rating numeric(2,1)
+);
+
+INSERT INTO Course(CourseID, CourseName, Rating) VALUES
+(1, 'Course 1', 4.5),
+(1, 'Course 2', 4.6),
+(1, 'Course 3', 4.7)
